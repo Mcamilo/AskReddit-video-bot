b30d8b1d1a0cf9a1b0882df23a1d1567e3d28142
Mcamilo <matheuscmilo@gmail.com>
2022-07-13 18:34:26 -0300

    (style) lint file and changed output location

--- robots/frames.js
+++ robots/frames.js
@@ -1,86 +1,94 @@
-var webshot = require('node-webshot');
-var fs      = require('fs');
-var state = require('./state.js')
-var DomParser = require('dom-parser');
+const fs = require("fs");
+const state = require("./state.js");
+const webshot = require("node-webshot");
 
-async function frames(){
-  console.log("Frames Bot...");
-  const questionHTML = fs.readFileSync('./resources/question.html', 'utf-8')
-  const commentHTML = fs.readFileSync('./resources/comment.html', 'utf-8')
-  const parser = new DomParser();
-  const date = new Date();
-  const timestamp = date.getTime();
+const templates_path = "./resources/templates";
+const questionHTML = fs.readFileSync(
+  `${templates_path}/question.html`,
+  "utf-8"
+);
+const commentHTML = fs.readFileSync(`${templates_path}/comment.html`, "utf-8");
 
-  const options = {
-    screenSize: {
+const date = new Date();
+const timestamp = date.getTime();
+const resultFramesPath = "./result/frames";
+const options = {
+  screenSize: {
     width: 1920,
-    height: 1080
-    },
-    siteType:'html'
-  }
-  const content = state.load()
-  const dir = content.id
+    height: 1080,
+  },
+  siteType: "html",
+};
+const content = state.load();
+const dir = content.id;
 
-  await getQuestionFrame()
-  await getCommentsFrames()
-  state.save(content)
-  async function getQuestionFrame(){
-    filepath = './frames/'+dir+'/'+content.id+'.png'
-    let fileReplaced = questionHTML.replace('QUESTION', content.title)
-    fileReplaced = fileReplaced.replace('AUTHOR', content.author)
-    fileReplaced = fileReplaced.replace('UPS', kFormatter(content.ups))
-    // fileReplaced = fileReplaced.replace('COMMENTS_NUM', kFormatter(content.numComments))
-    fileReplaced = fileReplaced.replace('CREATED', getDate(content.created))
-
-    await webshotPromise(fileReplaced, filepath, options)
-  }
-
-  async function getCommentsFrames(){
-    for (const comment of content.comments) {
-      var concatSentence = ''
-      var i = 0
-      var sentenceList = []
-      for (const sentence of comment.sentences) {
-        concatSentence += sentence.replace(/\n/g, "<br/>")
-        i++
-        filepath = './frames/'+dir+'/'+comment.id+'-'+i+'.png'
-        let fileReplaced = commentHTML.replace('AUTOR', comment.author)
-        fileReplaced = fileReplaced.replace('UPS', kFormatter(comment.ups))
-        fileReplaced = fileReplaced.replace('CREATED', getDate(comment.created))
-        fileReplaced = fileReplaced.replace('SENTENCE', concatSentence)
-        fileReplaced = fileReplaced.replace('FULL', comment.body.replace(/\n/g, "<br/>"))
-
-        await webshotPromise(fileReplaced, filepath, options)
-        sentenceList.push({'body':sentence, 'path': filepath})
-      }
-      comment.frames = sentenceList
-    }
-  }
-
-  async function webshotPromise(html, screenPath, options) {
-    let promise = new Promise((resolve, reject) => {
-      webshot(
-        html,
-        screenPath,
-        options,
-        e => (!e ? resolve(screenPath) : reject(e))
-      )
-    })
-    return promise
-  }
-  function getDate(created){
-    resolution = timestamp - created*1000
-    var resolutionTime = Math.floor((((resolution / 1000) / 60)/ 60))
-    if (resolutionTime > 23){
-      return Math.round(resolutionTime/24) + ' days'
-    }
-    return resolutionTime + ' hours'
-  }
-
-  function kFormatter(num) {
-    return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'k' : Math.sign(num)*Math.abs(num)
-  }
-
+async function frames() {
+  console.log(">[frames] Starting...");
+  await getQuestionFrame();
+  await getCommentsFrames();
+  state.save(content);
 }
 
-module.exports = frames
+async function getQuestionFrame() {
+  filepath = `${resultFramesPath}/${dir}/${content.id}.png`;
+  let fileReplaced = questionHTML.replace("QUESTION", content.title);
+  fileReplaced = fileReplaced.replace("AUTHOR", content.author);
+  fileReplaced = fileReplaced.replace("UPS", kFormatter(content.ups));
+  fileReplaced = fileReplaced.replace(
+    "COMMENTS_NUM",
+    kFormatter(content.numComments)
+  );
+  fileReplaced = fileReplaced.replace("CREATED", getDate(content.created));
+
+  await webshotPromise(fileReplaced, filepath, options);
+}
+
+async function getCommentsFrames() {
+  for (const comment of content.comments) {
+    var concatSentence = "";
+    var i = 0;
+    var sentenceList = [];
+    for (const sentence of comment.sentences) {
+      concatSentence += sentence.replace(/\n/g, "<br/>");
+      i++;
+      filepath = `${resultFramesPath}/${dir}/${comment.id}-${i}.png`;
+      let fileReplaced = commentHTML.replace("AUTOR", comment.author);
+      fileReplaced = fileReplaced.replace("UPS", kFormatter(comment.ups));
+      fileReplaced = fileReplaced.replace("CREATED", getDate(comment.created));
+      fileReplaced = fileReplaced.replace("SENTENCE", concatSentence);
+      fileReplaced = fileReplaced.replace(
+        "FULL",
+        comment.body.replace(/\n/g, "<br/>")
+      );
+
+      await webshotPromise(fileReplaced, filepath, options);
+      sentenceList.push({ body: sentence, path: filepath });
+    }
+    comment.frames = sentenceList;
+  }
+}
+
+async function webshotPromise(html, screenPath, options) {
+  let promise = new Promise((resolve, reject) => {
+    webshot(html, screenPath, options, (e) =>
+      !e ? resolve(screenPath) : reject(e)
+    );
+  });
+  return promise;
+}
+function getDate(created) {
+  resolution = timestamp - created * 1000;
+  var resolutionTime = Math.floor(resolution / 1000 / 60 / 60);
+  if (resolutionTime > 23) {
+    return Math.round(resolutionTime / 24) + " days";
+  }
+  return resolutionTime + " hours";
+}
+
+function kFormatter(num) {
+  return Math.abs(num) > 999
+    ? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + "k"
+    : Math.sign(num) * Math.abs(num);
+}
+
+module.exports = frames;
